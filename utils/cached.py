df72bc5433e87600d39931e2cd6ae29d18e3aa31
Hantao Cui <hcui7@vols.utk.edu>
2017-02-28 20:42:55 -0500

    Updated utility functions for lazy descriptions(cached) and table drawing(tab)

---
+++ utils/cached.py
@@ -0,0 +1,36 @@
+_missing = object()
+
+
+class cached(object):
+    """A decorator that converts a function into a lazy property.  The
+    function wrapped is called the first time to retrieve the result
+    and then that calculated result is used the next time you access
+    the value::
+
+        class Foo(object):
+
+            @cached_property
+            def foo(self):
+                # calculate something important here
+                return 42
+
+    The class has to have a `__dict__` in order for this property to
+    work.
+    See for details:
+    http://stackoverflow.com/questions/17486104/python-lazy-loading-of-class-attributes
+    """
+
+    def __init__(self, func, name=None, doc=None):
+        self.__name__ = name or func.__name__
+        self.__module__ = func.__module__
+        self.__doc__ = doc or func.__doc__
+        self.func = func
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        value = obj.__dict__.get(self.__name__, _missing)
+        if value is _missing:
+            value = self.func(obj)
+            obj.__dict__[self.__name__] = value
+        return value
