91af3a8a00d9b624196207c2190b0b9d5ac3938c
cuihantao <hcui7@vols.utk.edu>
2018-04-11 14:55:07 -0400

    Good parameter for eigenvalue, but VSC 4 needs a closer look
    Signed-off-by: cuihantao <hcui7@vols.utk.edu>

--- andes/tests/ieee39/gameopt.py
+++ andes/tests/ieee39/gameopt.py
@@ -2,7 +2,7 @@
 def pert(t, system):
     if abs(t - 2.0) < 1e-1:
         pass
-        if system.Syn6a.u[1] == 1:
-            system.Syn6a.u[1] = 0
-            system.Log.info('Generator 2 tripped at t = 2')
+        if system.Syn6a.u[2] == 1:
+            system.Syn6a.u[2] = 0
+            system.Log.info('Generator 3 tripped at t = 2')
             pass
