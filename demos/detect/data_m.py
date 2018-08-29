a85fa40d44e2dd9d91f5187ff89c977dadc21ac2
ZQW <qzhang41@vols.utk.edu>
2018-08-29 14:58:41 -0400

    <output generated adding time step>

---
+++ demos/detect/data_m.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import glob
+from numpy import *
+import pandas as pd
+import numpy as np
+# os.system('python run.py')
+# os.system('andes GT_* --ncpu=3')
+os.chdir('C:/Users/zhan2/PycharmProjects/andes_github/demos/detect/GT')
+bus_count=995
+for filename in glob.glob('*.dat'):
+    with open (filename) as f:
+        reader = csv.reader(f, delimiter="\t")
+        raw_data=list(reader)
+    row_count = sum(1 for row in raw_data)
+    frequency=mat(zeros((row_count, bus_count)))
+    voltage = mat(zeros((row_count, bus_count)))
+    time=mat(zeros((row_count,1)))
+    for idx,line in enumerate(raw_data):
+            ddc=line[0].split()
+            time[idx,0]=ddc[0]
+            for idx_2,freq_idx in enumerate(range(996,1991)):
+                frequency[idx,idx_2]=ddc[freq_idx]
+            for idx_3, vol_idx in enumerate(range(3981, 4976)):
+                voltage[idx, idx_3] = ddc[vol_idx]
+            df=pd.DataFrame(voltage)
+            df.to_csv('%s_voltage.csv' % filename)
+            df=pd.DataFrame(frequency)
+            df.to_csv('%s_frequency.csv' % filename)
+            output_file=open('%s.csv' % filename,'w')
+            with output_file:
+                writer = csv.writer(output_file)
+                writer.writerows()
+            print('pp')
+                # frequency(idx).append(ddc[bus_idx])
+                # bus_idx.append(idx)
+                # voltage(idx).append(ddc[])
+
+# with open ('GT_100121_Syn2_5_out.dat') as f:
+#     reader = csv.reader(f, delimiter="\t")
+#     for line in reader:
+#         print(line[7])
