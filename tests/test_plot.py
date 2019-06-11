0ebeea5808caa7037321c6e2e949846d3996c1e6
Cui, Hantao <cuihantao@gmail.com>
2019-06-11 11:29:12 -0400

    Updated `test_plot` for the new parse_y

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -13,37 +13,37 @@ class TestPlotParseYNew(unittest.TestCase):
         self.assertListEqual(andes.plot.parse_y(['1:3:'], 5),
                              [1, 2])
         self.assertListEqual(andes.plot.parse_y(['1:10'], 5),
-                             [1, 2, 3, 4, 5])
+                             [1, 2, 3, 4])
         self.assertListEqual(andes.plot.parse_y(['1:10:2'], 5),
-                             [1, 3, 5])
+                             [1, 3])
 
         self.assertListEqual(andes.plot.parse_y(['-2:3'], 5),
-                             [1, 2])
+                             [0, 1, 2])
         self.assertListEqual(andes.plot.parse_y(['-3:10'], 5),
-                             [1, 2, 3, 4, 5])
+                             [0, 1, 2, 3, 4])
         self.assertListEqual(andes.plot.parse_y(['-5:10:2'], 5),
-                             [1, 3, 5])
+                             [1, 3])
 
         self.assertListEqual(andes.plot.parse_y(['5:0:-1'], 5),
-                             [5, 4, 3, 2, 1])
+                             [4, 3, 2, 1])
         self.assertListEqual(andes.plot.parse_y(['10:1:'], 5),
                              [])
 
         self.assertListEqual(andes.plot.parse_y(['-1:999:'], 5),
-                             [1, 2, 3, 4, 5])
+                             [0, 1, 2, 3, 4])
         self.assertListEqual(andes.plot.parse_y(['-999:-1:'], 5),
-                             [1, 2, 3, 4, 5])
+                             [])
         self.assertListEqual(andes.plot.parse_y(['-999:-1:2'], 5),
-                             [1, 3, 5])
+                             [])
 
         self.assertListEqual(andes.plot.parse_y(['-99s9:-1:2'], 5),
                              [])
 
-        self.assertListEqual(andes.plot.parse_y(['1', '5'], 5),
-                             [1, 5])
+        self.assertListEqual(andes.plot.parse_y(['1', '4'], 5),
+                             [1, 4])
 
-        self.assertListEqual(andes.plot.parse_y(['1', '5', '10'], 5),
-                             [1, 5])
+        self.assertListEqual(andes.plot.parse_y(['1', '4', '5', '10'], 5),
+                             [1, 4])
 
-        self.assertListEqual(andes.plot.parse_y(['1', '5', '10'], 5, lower=2),
-                             [5])
+        self.assertListEqual(andes.plot.parse_y(['1', '4', '5', '10'], 5, lower=2),
+                             [4])
