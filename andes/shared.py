76b371babbb36edd81e7e0b1ec4535d90d738619
Hantao Cui <hcui7@utk.edu>
2019-12-29 19:30:57 -0500

    Reverted LazyImport due to large overhead

--- andes/shared.py
+++ andes/shared.py
@@ -9,38 +9,53 @@ Known issues ::
     got 216 from PyObject
         return f(*args, **kwds)
 """
-from andes.utils.lazyimport import LazyImport
+# from andes.utils.lazyimport import LazyImport
+#
+# # ----------------------------------------
+# # Packages
+# np = LazyImport('import numpy')
+# pd = LazyImport('import pandas')
+# tqdm = LazyImport('from tqdm import tqdm')
+# plt = LazyImport('from matplotlib import pyplot')
+# mpl = LazyImport('import matplotlib')
+# umfpack = LazyImport('from cvxopt import umfpack')
+# cvxopt = LazyImport('import cvxopt')
+# coloredlogs = LazyImport('import coloredlogs')
+# Process = LazyImport('from multiprocessing import Process')
+# # ----------------------------------------
+#
+# # ----------------------------------------
+# # Types
+# ndarray = LazyImport('from numpy import ndarray')
+# spmatrix = LazyImport('from cvxopt import spmatrix')
+# matrix = LazyImport('from cvxopt import matrix')
+# sparse = LazyImport('from cvxopt import sparse')
+# spdiag = LazyImport('from cvxopt import spdiag')
+# # ----------------------------------------
+#
+# # ----------------------------------------
+# # function calls
+# newton_krylov = LazyImport('from scipy.optimize import newton_krylov')
+# fsolve = LazyImport('from scipy.optimize import fsolve')
+# solve_ivp = LazyImport('from scipy.integrate import solve_ivp')
+# odeint = LazyImport('from scipy.integrate import odeint')
+# # ----------------------------------------
 
-# ----------------------------------------
-# Packages
-np = LazyImport('import numpy')
-pd = LazyImport('import pandas')
-tqdm = LazyImport('from tqdm import tqdm')
-plt = LazyImport('from matplotlib import pyplot')
-mpl = LazyImport('import matplotlib')
-umfpack = LazyImport('from cvxopt import umfpack')
-cvxopt = LazyImport('import cvxopt')
-coloredlogs = LazyImport('import coloredlogs')
-Process = LazyImport('from multiprocessing import Process')
-# ----------------------------------------
+import numpy as np  # NOQA
+import pandas as pd  # NOQA
+from tqdm import tqdm  # NOQA
+from matplotlib import pyplot as plt  # NOQA
+import matplotlib as mpl  # NOQA
+from cvxopt import umfpack  # NOQA
+import cvxopt  # NOQA
+import coloredlogs  # NOQA
+from multiprocessing import Process  # NOQA
 
-# ----------------------------------------
-# Types
-ndarray = LazyImport('from numpy import ndarray')
-spmatrix = LazyImport('from cvxopt import spmatrix')
-matrix = LazyImport('from cvxopt import matrix')
-sparse = LazyImport('from cvxopt import sparse')
-spdiag = LazyImport('from cvxopt import spdiag')
-# ----------------------------------------
+from numpy import ndarray  # NOQA
+from cvxopt import spmatrix, matrix, sparse, spdiag  # NOQA
 
-# ----------------------------------------
-# function calls
-newton_krylov = LazyImport('from scipy.optimize import newton_krylov')
-fsolve = LazyImport('from scipy.optimize import fsolve')
-solve_ivp = LazyImport('from scipy.integrate import solve_ivp')
-odeint = LazyImport('from scipy.integrate import odeint')
-# ----------------------------------------
-
+from scipy.optimize import fsolve, newton_krylov  # NOQA
+from scipy.integrate import solve_ivp, odeint  # NOQA
 
 pi = 3.14159265358973
 jpi2 = 1.5707963267948966j
