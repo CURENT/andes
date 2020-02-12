cc21a2f1a9ece05a34dfd931a860027f6621e256
Hantao Cui <hcui7@utk.edu>
2020-02-12 11:12:59 -0500

    Temporarily removed unsupported test cases

--- cases/ieee39/nompc39.dm
+++
@@ -1,4 +0,0 @@
-# DOME format version 1.0
-
-INCLUDE, ieee39_area.dm
-
--- cases/pert.py
+++
@@ -1,6 +0,0 @@
-"""Sample perturbation file. It trips the 9th generator at t=2"""
-
-
-def pert(t, system):
-    if 2.0 <= t <=2.2:
-        pass
