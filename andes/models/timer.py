b401720776b87e009c3c6c020870c73874688079
Hantao Cui <hcui7@utk.edu>
2020-04-10 23:16:10 -0400

    Print idx of Fault and Toggler in message

--- andes/models/timer.py
+++ andes/models/timer.py
@@ -35,8 +35,8 @@ class Toggler(TogglerData, Model):
                 u0 = instance.get(src='u', attr='v', idx=self.dev.v[i])
                 instance.set(src='u', attr='v', idx=self.dev.v[i], value=1-u0)
                 action = True
-                tqdm.write(f'<Toggle {i}>: Status of {self.model.v[i]}.{self.dev.v[i]} changed to {1-u0} at '
-                           f't={self.t.v[i]}sec.')
+                tqdm.write(f'<Toggle {self.idx[i]}>: '
+                           f'Status of {self.model.v[i]}.{self.dev.v[i]} changed to {1-u0} at t={self.t.v[i]}sec.')
         return action
 
 
@@ -112,7 +112,8 @@ class Fault(ModelData, Model):
                 self.uf.v[i] = 1
                 # store voltages along with the rest of algebraic variables
                 self._vstore = np.array(self.system.dae.y[self.system.Bus.n:])
-                tqdm.write(f'<Fault {i}>: Applying fault on Bus (idx={self.bus.v[i]}) at t={self.tf.v[i]}sec.')
+                tqdm.write(f'<Fault {self.idx[i]}>: '
+                           f'Applying fault on Bus (idx={self.bus.v[i]}) at t={self.tf.v[i]}sec.')
                 return True
         return False
 
@@ -124,6 +125,7 @@ class Fault(ModelData, Model):
             if is_time[i] and (self.u.v[i] == 1):
                 self.uf.v[i] = 0
                 self.system.dae.y[self.system.Bus.n:] = self._vstore
-                tqdm.write(f'<Fault {i}>: Clearing fault on Bus (idx={self.bus.v[i]}) at t={self.tc.v[i]}sec.')
+                tqdm.write(f'<Fault {self.idx[i]}>: '
+                           f'Clearing fault on Bus (idx={self.bus.v[i]}) at t={self.tc.v[i]}sec.')
                 return True
         return False
