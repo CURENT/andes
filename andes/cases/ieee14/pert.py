9750d557c4ec5d0f272efc55f4c58ebaaaec2a64
Hantao Cui <cuihantao@gmail.com>
2020-11-01 17:55:37 -0500

    Updated docs.

--- andes/cases/ieee14/pert.py
+++ andes/cases/ieee14/pert.py
@@ -1,7 +1,14 @@
 """
-Empty pert file
+An empty pert file.
 """
 
 
 def pert(t, system):
+    """
+    Perturbation function called at each step.
+
+    The function is named "pert" and takes two arguments:
+    ``t`` for simulation time, and ``system`` for the system object.
+
+    """
     pass
