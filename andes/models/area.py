c0e7ab1f7adeee765cb13c8e42b099d80825bda6
Hantao Cui <cuihantao@gmail.com>
2020-11-03 21:34:11 -0500

    Removed unneeded functions.

--- andes/models/area.py
+++ andes/models/area.py
@@ -15,6 +15,12 @@ class AreaData(ModelData):
 
 
 class Area(AreaData, Model):
+    """
+    Area model.
+
+    Area collects back references from the Bus model and
+    the ACTopology group.
+    """
     def __init__(self, system, config):
         AreaData.__init__(self)
         Model.__init__(self, system, config)
@@ -24,18 +30,6 @@ class Area(AreaData, Model):
 
         self.Bus = BackRef()
         self.ACTopology = BackRef()
-
-        # --------------------Experiment Zone--------------------
-        self.Vn = ExtParam(model='Bus', src='Vn', indexer=self.ACTopology, export=False)
-        self.Vn_sum = NumReduce(u=self.Vn, fun=np.sum, ref=self.Bus)
-        self.Vn_sum_rep = NumRepeat(u=self.Vn_sum, ref=self.Bus)
-
-        self.a = ExtAlgeb(model='ACTopology', src='a', indexer=self.ACTopology,
-                          info='Bus voltage angle')
-        self.v = ExtAlgeb(model='ACTopology', src='v', indexer=self.ACTopology,
-                          info='Bus voltage magnitude')
-
-        # self.time = Algeb(e_str='time - dae_t', v_setter=True)
 
     def bus_table(self):
         """
