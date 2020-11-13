4ac0e80f2dcb5a9fd3889dc0f6c95a8a65f0c9d7
Hantao Cui <cuihantao@gmail.com>
2020-11-12 23:51:28 -0500

    Minor edits.

--- andes/core/param.py
+++ andes/core/param.py
@@ -381,13 +381,13 @@ class NumParam(BaseParam):
         if isinstance(value, float):
             # check for non-zero
             if value == 0.0 and self.get_property('non_zero'):
-                logger.warning(f'Non-zero parameter %s.%s corrected to %s',
+                logger.warning('Non-zero parameter %s.%s corrected to %s',
                                self.owner.class_name, self.name, self.default)
                 value = self.default
 
             # check for positive
             if value <= 0.0 and self.get_property('positive'):
-                logger.warning(f'Positive parameter %s.%s corrected to %s',
+                logger.warning('Positive parameter %s.%s corrected to %s',
                                self.owner.class_name, self.name, self.default)
                 value = self.default
 
