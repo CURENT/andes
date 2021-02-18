3fb57be5df779be3979cd6190ce1d7b955d3875c
Hantao Cui <cuihantao@gmail.com>
2021-02-18 16:10:24 -0500

    Refactor renewable models into a module.

---
+++ andes/models/renewable/regca1.py
@@ -0,0 +1,269 @@
+from andes.core import ModelData, IdxParam, NumParam, Model, ExtAlgeb, ExtService, ConstService, ExtParam, Algeb, \
+    Piecewise, Lag
+from andes.core.block import LagAntiWindupRate, GainLimiter
+
+
+class REGCA1Data(ModelData):
+    """
+    REGC_A model data.
+
+    12/09/2020: Made `Lvplsw` a parameter.
+    """
+
+    def __init__(self):
+        ModelData.__init__(self)
+
+        self.bus = IdxParam(model='Bus',
+                            info="interface bus id",
+                            mandatory=True,
+                            )
+        self.gen = IdxParam(info="static generator index",
+                            mandatory=True,
+                            )
+        self.Sn = NumParam(default=100.0, tex_name='S_n',
+                           info='Model MVA base',
+                           unit='MVA',
+                           )
+
+        self.Tg = NumParam(default=0.1, tex_name='T_g',
+                           info='converter time const.', unit='s',
+                           )
+        self.Rrpwr = NumParam(default=10.0, tex_name='R_{rpwr}',
+                              info='Low voltage power logic (LVPL) ramp limit',
+                              unit='p.u.',
+                              )
+        self.Brkpt = NumParam(default=1.0, tex_name='B_{rkpt}',
+                              info='LVPL characteristic voltage 2',
+                              unit='p.u.',
+                              )
+        self.Zerox = NumParam(default=0.5, tex_name='Z_{erox}',
+                              info='LVPL characteristic voltage 1',
+                              unit='p.u',
+                              )
+        # TODO: ensure Brkpt > Zerox
+        self.Lvplsw = NumParam(default=1.0, tex_name='z_{Lvplsw}',
+                               info='Low volt. P logic: 1-enable, 0-disable',
+                               unit='bool',
+                               )
+
+        self.Lvpl1 = NumParam(default=1.0, tex_name='L_{vpl1}',
+                              info='LVPL gain',
+                              unit='p.u',
+                              )
+        self.Volim = NumParam(default=1.2, tex_name='V_{olim}',
+                              info='Voltage lim for high volt. reactive current mgnt.',
+                              unit='p.u.',
+                              )
+        self.Lvpnt1 = NumParam(default=0.8, tex_name='L_{vpnt1}',
+                               info='High volt. point for low volt. active current mgnt.',
+                               unit='p.u.',
+                               )
+        self.Lvpnt0 = NumParam(default=0.4, tex_name='L_{vpnt0}',
+                               info='Low volt. point for low volt. active current mgnt.',
+                               unit='p.u.',
+                               )
+        # TODO: ensure Lvpnt1 > Lvpnt0
+        self.Iolim = NumParam(default=-1.5, tex_name='I_{olim}',
+                              info='lower current limit for high volt. reactive current mgnt.',
+                              unit='p.u. (mach base)',
+                              current=True,
+                              )
+        self.Tfltr = NumParam(default=0.1, tex_name='T_{fltr}',
+                              info='Voltage filter T const for low volt. active current mgnt.',
+                              unit='s',
+                              )
+        self.Khv = NumParam(default=0.7, tex_name='K_{hv}',
+                            info='Overvolt. compensation gain in high volt. reactive current mgnt.',
+                            )
+        self.Iqrmax = NumParam(default=1, tex_name='I_{qrmax}',
+                               info='Upper limit on the ROC for reactive current',
+                               unit='p.u.',
+                               current=True,
+                               )
+        self.Iqrmin = NumParam(default=-1, tex_name='I_{qrmin}',
+                               info='Lower limit on the ROC for reactive current',
+                               unit='p.u.',
+                               current=True,
+                               )
+        self.Accel = NumParam(default=0.0, tex_name='A_{ccel}',
+                              info='Acceleration factor',
+                              vrange=(0, 1.0),
+                              )
+        self.gammap = NumParam(default=1.0,
+                               info="P ratio of linked static gen",
+                               tex_name=r'\gamma_P'
+                               )
+        self.gammaq = NumParam(default=1.0,
+                               info="Q ratio of linked static gen",
+                               tex_name=r'\gamma_Q'
+                               )
+
+
+class REGCA1Model(Model):
+    """
+    REGCA1 implementation.
+    """
+
+    def __init__(self, system, config):
+        Model.__init__(self, system, config)
+        self.flags.tds = True
+        self.group = 'RenGen'
+
+        self.a = ExtAlgeb(model='Bus',
+                          src='a',
+                          indexer=self.bus,
+                          tex_name=r'\theta',
+                          info='Bus voltage angle',
+                          e_str='-Pe',
+                          )
+
+        self.v = ExtAlgeb(model='Bus',
+                          src='v',
+                          indexer=self.bus,
+                          tex_name=r'V',
+                          info='Bus voltage magnitude',
+                          e_str='-Qe',
+                          )
+
+        self.p0s = ExtService(model='StaticGen',
+                              src='p',
+                              indexer=self.gen,
+                              tex_name='P_{0s}',
+                              info='initial P of the static gen',
+                              )
+        self.q0s = ExtService(model='StaticGen',
+                              src='q',
+                              indexer=self.gen,
+                              tex_name='Q_{0s}',
+                              info='initial Q of the static gen',
+                              )
+        self.p0 = ConstService(v_str='p0s * gammap',
+                               tex_name='P_0',
+                               info='initial P of this gen',
+                               )
+        self.q0 = ConstService(v_str='q0s * gammaq',
+                               tex_name='Q_0',
+                               info='initial Q of this gen',
+                               )
+        self.ra = ExtParam(model='StaticGen',
+                           src='ra',
+                           indexer=self.gen,
+                           tex_name='r_a',
+                           export=False,
+                           )
+        self.xs = ExtParam(model='StaticGen',
+                           src='xs',
+                           indexer=self.gen,
+                           tex_name='x_s',
+                           export=False,
+                           )
+
+        # --- INITIALIZATION ---
+        self.q0gt0 = ConstService('Indicator(q0> 0)', tex_name='z_{q0>0}',
+                                  info='flags for q0 below zero',
+                                  )
+        self.q0lt0 = ConstService('Indicator(q0< 0)', tex_name='z_{q0<0}',
+                                  info='flags for q0 below zero',
+                                  )
+
+        self.Ipcmd0 = ConstService('p0 / v', info='initial Ipcmd',
+                                   tex_name='I_{pcmd0}',
+                                   )
+
+        self.Iqcmd0 = ConstService('-q0 / v', info='initial Iqcmd',
+                                   tex_name='I_{qcmd0}',
+                                   )
+
+        self.Ipcmd = Algeb(tex_name='I_{pcmd}',
+                           info='current component for active power',
+                           e_str='Ipcmd0 - Ipcmd', v_str='Ipcmd0')
+
+        self.Iqcmd = Algeb(tex_name='I_{qcmd}',
+                           info='current component for reactive power',
+                           e_str='Iqcmd0 - Iqcmd', v_str='Iqcmd0')
+
+        # reactive power management
+
+        # rate limiting logic (for fault recovery, although it does not detect any recovery)
+        #   - activate upper limit when q0 > 0 (self.q0gt0)
+        #   - activate lower limit when q0 < 0 (self.q0lt0)
+
+        self.S1 = LagAntiWindupRate(u=self.Iqcmd,
+                                    T=self.Tg, K=-1,
+                                    lower=-9999, upper=9999, no_lower=True, no_upper=True,
+                                    rate_lower=self.Iqrmin, rate_upper=self.Iqrmax,
+                                    rate_lower_cond=self.q0lt0, rate_upper_cond=self.q0gt0,
+                                    tex_name='S_1',
+                                    info='Iqcmd delay',
+                                    )  # output `S1_y` == `Iq`
+
+        # piece-wise gain for low voltage active current mgnt.
+        self.kLVG = ConstService(v_str='1 / (Lvpnt1 - Lvpnt0)',
+                                 tex_name='k_{LVG}',
+                                 )
+
+        self.LVG = Piecewise(u=self.v, points=('Lvpnt0', 'Lvpnt1'),
+                             funs=('0', '(v - Lvpnt0) * kLVG', '1'),
+                             info='Ip gain during low voltage',
+                             tex_name='L_{VG}',
+                             )
+
+        # piece-wise gain for LVPL
+        self.kLVPL = ConstService(v_str='Lvplsw * Lvpl1 / (Brkpt - Zerox)',
+                                  tex_name='k_{LVPL}',
+                                  )
+
+        self.S2 = Lag(u=self.v, T=self.Tfltr, K=1.0,
+                      info='Voltage filter with no anti-windup',
+                      tex_name='S_2',
+                      )
+        self.LVPL = Piecewise(u=self.S2_y,
+                              points=('Zerox', 'Brkpt'),
+                              funs=('0 + 9999*(1-Lvplsw)',
+                                    '(S2_y - Zerox) * kLVPL + 9999 * (1-Lvplsw)',
+                                    '9999'),
+                              info='Low voltage Ipcmd upper limit',
+                              tex_name='L_{VPL}',
+                              )
+
+        self.S0 = LagAntiWindupRate(u=self.Ipcmd, T=self.Tg, K=1,
+                                    upper=self.LVPL_y, rate_upper=self.Rrpwr,
+                                    lower=-9999, rate_lower=-9999,
+                                    no_lower=True, rate_no_lower=True,
+                                    tex_name='S_0',
+                                    )  # `S0_y` is the output `Ip` in the block diagram
+
+        self.Ipout = Algeb(e_str='S0_y * LVG_y -Ipout',
+                           v_str='Ipcmd * LVG_y',
+                           info='Output Ip current',
+                           tex_name='I_{pout}',
+                           )
+
+        # high voltage part
+        self.HVG = GainLimiter(u='v - Volim', K=self.Khv, info='High voltage gain block',
+                               lower=0, upper=999, no_upper=True,
+                               tex_name='H_{VG}'
+                               )
+        self.HVG.lim.no_warn = True
+
+        self.Iqout = GainLimiter(u='S1_y- HVG_y', K=1, lower=self.Iolim, upper=9999,
+                                 no_upper=True, info='Iq output block',
+                                 tex_name='I^{qout}',
+                                 )  # `Iqout_y` is the final Iq output
+
+        self.Pe = Algeb(tex_name='P_e', info='Active power output',
+                        v_str='p0', e_str='Ipout * v - Pe')
+        self.Qe = Algeb(tex_name='Q_e', info='Reactive power output',
+                        v_str='q0', e_str='Iqout_y * v - Qe')
+
+    def v_numeric(self, **kwargs):
+        """
+        Disable the corresponding `StaticGen`s.
+        """
+        self.system.groups['StaticGen'].set(src='u', idx=self.gen.v, attr='v', value=0)
+
+
+class REGCA1(REGCA1Data, REGCA1Model):
+    def __init__(self, system, config):
+        REGCA1Data.__init__(self)
+        REGCA1Model.__init__(self, system, config)
---
+++ andes/models/renewable/wttqa1.py
@@ -0,0 +1,256 @@
+import numpy as np
+
+from andes.core import ModelData, IdxParam, NumParam, Model, ConstService, ExtParam, ExtAlgeb, Lag, Piecewise, \
+    ExtState, ExtService, Switcher, Algeb
+from andes.core.block import PIAWHardLimit
+from andes.core.service import NumSelect
+
+
+class WTTQA1Data(ModelData):
+    """
+    Wind turbine torque (Pref) model.
+    """
+
+    def __init__(self):
+        ModelData.__init__(self)
+
+        self.rep = IdxParam(mandatory=True,
+                            info='RenPitch controller idx',
+                            )
+
+        self.Kip = NumParam(default=0.1, info='Pref-control integral gain',
+                            tex_name='K_{ip}',
+                            unit='p.u.',
+                            )
+
+        self.Kpp = NumParam(default=0.0, info='Pref-control proportional gain',
+                            tex_name='K_{pp}',
+                            unit='p.u.',
+                            )
+
+        self.Tp = NumParam(default=0.05, info='Pe sensing time const.',
+                           tex_name='T_p',
+                           unit='s',
+                           )
+
+        self.Twref = NumParam(default=30.0, info='Speed reference time const.',
+                              tex_name='T_{wref}',
+                              unit='s',
+                              vrange=(30, 60),
+                              )
+
+        self.Temax = NumParam(default=1.2, info='Max. electric torque',
+                              tex_name='T_{emax}',
+                              unit='p.u.',
+                              vrange=(1.1, 1.2),
+                              power=True,
+                              )
+
+        self.Temin = NumParam(default=0.0, info='Min. electric torque',
+                              tex_name='T_{emin}',
+                              unit='p.u.',
+                              power=True,
+                              )
+
+        self.Tflag = NumParam(info='Tflag; 1-power error, 0-speed error',
+                              mandatory=True,
+                              unit='bool',
+                              )
+
+        self.p1 = NumParam(default=0.2, info='Active power point 1',
+                           unit='p.u.', tex_name='p_1',
+                           power=True,
+                           )
+        self.sp1 = NumParam(default=0.58, info='Speed power point 1',
+                            unit='p.u.', tex_name='s_{p1}',
+                            )
+
+        self.p2 = NumParam(default=0.4, info='Active power point 2',
+                           unit='p.u.', tex_name='p_2',
+                           power=True,
+                           )
+        self.sp2 = NumParam(default=0.72, info='Speed power point 2',
+                            unit='p.u.', tex_name='s_{p2}',
+                            )
+
+        self.p3 = NumParam(default=0.6, info='Active power point 3',
+                           unit='p.u.', tex_name='p_3',
+                           power=True,
+                           )
+        self.sp3 = NumParam(default=0.86, info='Speed power point 3',
+                            unit='p.u.', tex_name='s_{p3}',
+                            )
+
+        self.p4 = NumParam(default=0.8, info='Active power point 4',
+                           unit='p.u.', tex_name='p_4',
+                           power=True,
+                           )
+        self.sp4 = NumParam(default=1.0, info='Speed power point 4',
+                            unit='p.u.', tex_name='s_{p4}',
+                            )
+        self.Tn = NumParam(default=np.nan, tex_name='T_n',
+                           info='Turbine rating. Use Sn from gov if none.',
+                           unit='MVA',
+                           )
+
+
+class WTTQA1Model(Model):
+    """
+    Wind turbine torque Pref model equations.
+    """
+
+    def __init__(self, system, config):
+        Model.__init__(self, system, config)
+
+        self.flags.tds = True
+        self.group = 'RenTorque'
+
+        self.kp1 = ConstService(v_str='(sp2 - sp1) / (p2 - p1)',
+                                tex_name='k_{p1}',
+                                )
+        self.kp2 = ConstService(v_str='(sp3 - sp2) / (p3 - p2)',
+                                tex_name='k_{p2}',
+                                )
+        self.kp3 = ConstService(v_str='(sp4 - sp3) / (p4 - p3)',
+                                tex_name='k_{p3}',
+                                )
+
+        self.rea = ExtParam(model='RenPitch', src='rea', indexer=self.rep, export=False,
+                            )
+
+        self.rego = ExtParam(model='RenAerodynamics', src='rego', indexer=self.rea,
+                             export=False,
+                             )
+
+        self.ree = ExtParam(model='RenGovernor', src='ree', indexer=self.rego,
+                            export=False,
+                            )
+
+        self.reg = ExtParam(model='RenExciter', src='reg', indexer=self.ree,
+                            export=False,)
+
+        self.Sngo = ExtParam(model='RenGovernor', src='Sn', indexer=self.rego,
+                             tex_name='S_{n,go}', export=False,
+                             )
+        self.Sn = NumSelect(self.Tn,
+                            fallback=self.Sngo,
+                            tex_name='S_n',
+                            info='Turbine or RenGovernor rating',
+                            )
+
+        self.Pe = ExtAlgeb(model='RenGen', src='Pe', indexer=self.reg,
+                           tex_name='P_e', export=False,
+                           )
+
+        self.s1 = Lag(u=self.Pe, T=self.Tp, K=1.0, tex_name='s_1',
+                      info='Pe filter',
+                      )
+
+        self.fPe = Piecewise(u=self.s1_y,
+                             points=('p1', 'p2', 'p3', 'p4'),
+                             funs=('sp1',
+                                   f'sp1 + ({self.s1_y.name} - p1) * kp1',
+                                   f'sp2 + ({self.s1_y.name} - p2) * kp2',
+                                   f'sp3 + ({self.s1_y.name} - p3) * kp3',
+                                   'sp4'),
+                             tex_name='f_{Pe}',
+                             info='Piecewise Pe to wref mapping',
+                             )
+
+        # Overwrite `wg` and `wt` initial values in turbine governors
+        self.wg = ExtState(model='RenGovernor', src='wg', indexer=self.rego,
+                           tex_name=r'\omega_g', export=False,
+                           v_str='fPe_y',
+                           v_setter=True,
+                           )
+
+        self.wt = ExtState(model='RenGovernor', src='wt', indexer=self.rego,
+                           tex_name=r'\omega_t', export=False,
+                           v_str='fPe_y',
+                           v_setter=True,
+                           )
+
+        self.s3_y = ExtState(model='RenGovernor', src='s3_y', indexer=self.rego,
+                             tex_name='y_{s3}', export=False,
+                             v_str='Pref0 / wg / Kshaft',
+                             v_setter=True,
+                             )
+
+        self.w0 = ExtParam(model='RenGovernor', src='w0', indexer=self.rego,
+                           tex_name=r'\omega_0', export=False,
+                           )
+
+        self.Kshaft = ExtService(model='RenGovernor', src='Kshaft', indexer=self.rego,
+                                 tex_name='K_{shaft}',
+                                 )
+
+        self.wr0 = ExtAlgeb(model='RenGovernor', src='wr0', indexer=self.rego,
+                            tex_name=r'\omega_{r0}', export=False,
+                            info='Retrieved initial w0 from RenGovernor',
+                            v_str='fPe_y',
+                            e_str='-w0 + fPe_y',
+                            v_setter=True,
+                            )
+
+        self.s2 = Lag(u=self.fPe_y, T=self.Twref, K=1.0,
+                      tex_name='s_2', info='speed filter',
+                      )
+
+        self.SWT = Switcher(u=self.Tflag, options=(0, 1),
+                            tex_name='SW_{T}',
+                            cache=True,
+                            )
+
+        self.Tsel = Algeb(tex_name='T_{sel}',
+                          info='Output after Tflag selector',
+                          discrete=self.SWT
+                          )
+        self.Tsel.v_str = 'SWT_s1 * (Pe - Pref0) / wg +' \
+                          'SWT_s0 * (s2_y - wg)'
+        self.Tsel.e_str = f'{self.Tsel.v_str} - Tsel'
+
+        self.PI = PIAWHardLimit(u=self.Tsel, kp=self.Kpp, ki=self.Kip,
+                                aw_lower=self.Temin, aw_upper=self.Temax,
+                                lower=self.Temin, upper=self.Temax,
+                                tex_name='PI',
+                                info='PI controller',
+                                x0='Pref0 / fPe_y',
+                                )
+
+        # Note:
+        #   Reset `wg` of REECA1 to 1.0 becase `wg` has already been multiplied
+        #   in the toeque model.
+        #   This effectively sets `PFLAG` to 0 if the torque model is connected.
+
+        self.wge = ExtAlgeb(model='RenExciter', src='wg', indexer=self.ree,
+                            tex_name=r'\omega_{ge}', export=False,
+                            v_str='1.0',
+                            e_str='-fPe_y + 1',
+                            v_setter=True,
+                            )
+
+        self.Pref0 = ExtService(model='RenExciter', src='p0', indexer=self.ree,
+                                tex_name='P_{ref0}',
+                                )
+
+        self.Pref = ExtAlgeb(model='RenExciter', src='Pref', indexer=self.ree,
+                             tex_name='P_{ref}', export=False,
+                             e_str='-Pref0 / wge + PI_y * wg',
+                             v_str='PI_y * wg',
+                             v_setter=True,
+                             )
+
+
+class WTTQA1(WTTQA1Data, WTTQA1Model):
+    """
+    Wind turbine generator torque (Pref) model.
+
+    PI state freeze following voltage dip has not been implemented.
+
+    Resets `wg` in `REECA1` model to 1.0 when torque model is connected.
+    This effectively ignores `PFLAG` of `REECA1`.
+    """
+
+    def __init__(self, config, system):
+        WTTQA1Data.__init__(self)
+        WTTQA1Model.__init__(self, system, config)
