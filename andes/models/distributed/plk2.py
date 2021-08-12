18d469787954e66c31196d4b87912adad0fea7f8
Jinning Wang <jwang175@CEPD00648.curent.utk.edu>
2021-08-12 00:57:57 -0400

    Modified equations.

--- andes/models/distributed/plk2.py
+++ andes/models/distributed/plk2.py
@@ -5,7 +5,7 @@ from andes.core.param import IdxParam, NumParam, ExtParam
 from andes.core.model import Model, ModelData
 from andes.core.var import Algeb, ExtAlgeb
 from andes.core.service import ConstService
-from andes.core.discrete import Limiter, Delay, HardLimiter
+from andes.core.discrete import Limiter, Delay
 
 
 class PLK2Data(ModelData):
@@ -148,7 +148,7 @@ class PLK2Data(ModelData):
                              )
         self.Tvu2 = NumParam(default=0.16,
                              tex_name=r't_{vu2}',
-                             info='Stand time for (vu1, vu2)',
+                             info='Stand time for (vu2, vu3)',
                              non_negative=True,
                              )
 
@@ -183,7 +183,7 @@ class PLK2Model(Model):
                           )
         self.fcvt = Algeb(v_str='fn',
                           e_str='fn * f - fcvt',
-                          info='Frequency  deviation indicator for (fl2, fl1)',
+                          info='Frequency deviation in nominal value',
                           tex_name='zs_{fdl1}',
                           )
 
@@ -192,12 +192,12 @@ class PLK2Model(Model):
                             lower=self.fl3,
                             upper=self.fl1,
                             tex_name=r'f_{cl1}',
-                            info='Frequency comparer for (fl2, fl1)',
+                            info='Frequency comparer for (fl3, fl1)',
                             equal=False,
                             )
         self.fdl1 = Algeb(v_str='0',
                           e_str='fcl1_zi - fdl1',
-                          info='Frequency  deviation indicator for (fl2, fl1)',
+                          info='Frequency  deviation indicator for (fl3, fl1)',
                           tex_name='zs_{fdl1}',
                           )
 
@@ -205,12 +205,12 @@ class PLK2Model(Model):
                             lower=self.fl3,
                             upper=self.fl2,
                             tex_name=r'f_{cl2}',
-                            info='Frequency comparer for (fl2, fl1)',
+                            info='Frequency comparer for (fl3, fl2)',
                             equal=False,
                             )
         self.fdl2 = Algeb(v_str='0',
                           e_str='fcl2_zi - fdl2',
-                          info='Frequency  deviation indicator for (fl2, fl1)',
+                          info='Frequency  deviation indicator for (fl3, fl2)',
                           tex_name='zs_{fdl2}',
                           )
 
@@ -218,12 +218,12 @@ class PLK2Model(Model):
                             lower=self.fu1,
                             upper=self.fu3,
                             tex_name=r'f_{cu1}',
-                            info='Frequency comparer for (fu1, fu2)',
+                            info='Frequency comparer for (fu1, fu3)',
                             equal=False,
                             )
         self.fdu1 = Algeb(v_str='0',
                           e_str='fcu1_zi - fdu1',
-                          info='Frequency  deviation indicator for (fu1, fu2)',
+                          info='Frequency  deviation indicator for (fu1, fu3)',
                           tex_name='zs_{fdu1}',
                           )
 
@@ -264,34 +264,34 @@ class PLK2Model(Model):
                           )
         # Indicatior of voltage deviation
         self.Vcl1 = Limiter(u=self.v,
-                            lower=self.ul4,
-                            upper=self.ul1,
+                            lower=self.vl4,
+                            upper=self.vl1,
                             tex_name=r'V_{cl1}',
-                            info='Voltage comparer for (vl2, vl1)',
+                            info='Voltage comparer for (vl4, vl1)',
                             equal=False,
                             )
         self.Vdl1 = Algeb(v_str='0',
                           e_str='Vcl1_zi - Vdl1',
-                          info='Voltage deviation indicator for (vl2, vl1)',
+                          info='Voltage deviation indicator for (vl4, vl1)',
                           tex_name='zs_{Vdl1}',
                           )
 
         self.Vcl2 = Limiter(u=self.v,
-                            lower=self.ul4,
-                            upper=self.ul2,
+                            lower=self.vl4,
+                            upper=self.vl2,
                             tex_name=r'V_{cl1}',
-                            info='Voltage comparer for (vl3, vl2)',
+                            info='Voltage comparer for (vl4, vl2)',
                             equal=False,
                             )
         self.Vdl2 = Algeb(v_str='0',
                           e_str='Vcl2_zi - Vdl2',
-                          info='Voltage deviation indicator for (vl3, vl2)',
+                          info='Voltage deviation indicator for (vl4, vl2)',
                           tex_name='zs_{Vdl1}',
                           )
 
         self.Vcl3 = Limiter(u=self.v,
-                            lower=self.ul4,
-                            upper=self.ul3,
+                            lower=self.vl4,
+                            upper=self.vl3,
                             tex_name=r'V_{cl3}',
                             info='Voltage comparer for (vl4, vl3)',
                             equal=False,
@@ -303,21 +303,21 @@ class PLK2Model(Model):
                           )
 
         self.Vcu1 = Limiter(u=self.v,
-                            lower=self.uu1,
-                            upper=self.uu3,
+                            lower=self.vu1,
+                            upper=self.vu3,
                             tex_name=r'V_{cu1}',
-                            info='Voltage comparer for (vu1, vu2)',
+                            info='Voltage comparer for (vu1, vu3)',
                             equal=False,
                             )
         self.Vdu1 = Algeb(v_str='0',
                           e_str='Vcu1_zi - Vdu1',
-                          info='Voltage deviation indicator for (vu1, vu2)',
-                          tex_name='zs_{Vdl1}',
+                          info='Voltage deviation indicator for (vu1, vu3)',
+                          tex_name=r'zs_{Vdl1}',
                           )
 
         self.Vcu2 = Limiter(u=self.v,
-                            lower=self.uu2,
-                            upper=self.uu3,
+                            lower=self.vu2,
+                            upper=self.vu3,
                             tex_name=r'V_{cu2}',
                             info='Voltage comparer for (vu2, vu3)',
                             equal=False,
@@ -325,7 +325,7 @@ class PLK2Model(Model):
         self.Vdu2 = Algeb(v_str='0',
                           e_str='Vcu2_zi - Vdu2',
                           info='Voltage deviation indicator for (vu2, vu3)',
-                          tex_name='zs_{Vdu2}',
+                          tex_name=r'zs_{Vdu2}',
                           )
 
         # Delayed voltage deviation indicator
@@ -347,59 +347,71 @@ class PLK2Model(Model):
 
         # -- Lock PVD1 output power
 
-        self.ltu = ConstService(v_str='1')
-        self.ltl = ConstService(v_str='0')
+        self.ltu = ConstService(v_str='0.8')
+        self.ltl = ConstService(v_str='0.2')
 
         # frequency
         self.fp = Algeb(v_str='0',
                         info='Frequency protection signal',
-                        tex_name=r'fp',
+                        tex_name=r'f_p',
                         )
         self.fp.e_str = 'fena * (fdl1d_v * fdl1 + fdl2d_v * fdl2 + \
                                  fdu1d_v * fdu1 + fdu2d_v * fdu2) - fp'
-        self.fphl = HardLimiter(u=self.fp,
-                                lower=self.ltl,
-                                upper=self.ltu,
-                                tex_name=r'fp_{hl}',
-                                )
+        self.fpc = Limiter(u=self.fp,
+                           lower=self.ltl,
+                           upper=self.ltu,
+                           )
+
         # voltage
         self.Vp = Algeb(v_str='0',
                         info='Voltage protection signal',
-                        tex_name=r'Vp',
+                        tex_name=r'V_p',
                         )
         self.Vp.e_str = 'Vena * (Vdl1d_v * Vdl1 + Vdl2d_v * Vdl2 + \
                                  Vdl3d_v * Vdl3 + Vdu1d_v * Vdu1 + \
                                  Vdu2d_v * Vdu2) - Vp'
-        self.Vphl = HardLimiter(u=self.Vp,
-                                lower=self.ltu,
-                                upper=self.ltl,
-                                tex_name=r'Vp_{hl}',
-                                )
+        self.Vpc = Limiter(u=self.Vp,
+                           lower=self.ltl,
+                           upper=self.ltu,
+                           )
 
         # lock actvie and reactive power output
-        self.pplk = ExtAlgeb(model='DG',
+        self.fplk = ExtAlgeb(model='DG',
                              src='Psum',
                              indexer=self.dev,
                              export=False,
-                             e_str='-1000 * pplk * fp',
-                             info='Active power locker',
+                             e_str='-100 * fplk * fpc_zu',
+                             info='Active power locker for frequency',
                              )
-        self.qplk = ExtAlgeb(model='DG',
+        self.fqlk = ExtAlgeb(model='DG',
                              src='Qsum',
                              indexer=self.dev,
                              export=False,
-                             e_str='-1000 * qplk * Vp',
-                             info='Reactive power locker',
+                             e_str='-100 * fqlk * fpc_zu',
+                             info='Reactive power locker for frequency',
+                             )
+
+        self.Vplk = ExtAlgeb(model='DG',
+                             src='Psum',
+                             indexer=self.dev,
+                             export=False,
+                             e_str='-100 * Vplk * Vpc_zu',
+                             info='Active power locker for voltage',
+                             )
+        self.Vqlk = ExtAlgeb(model='DG',
+                             src='Qsum',
+                             indexer=self.dev,
+                             export=False,
+                             e_str='-100 * Vqlk * Vpc_zu',
+                             info='Reactive power locker for voltage',
                              )
 
 
 class PLK2(PLK2Data, PLK2Model):
     """
-    DER protection model type 2. PLK stands for Power Lock.
+    DER protection model type 2, derived from PLK, followed IEEE-1547. PLK stands for Power Lock.
 
-    Derived from PLK, followed IEEE-1547.
-
-    Target device (limited to DG group) ``Psum`` and ``Qsum`` will drop to zero
+    Target device (limited to DG group) ``Psum`` and ``Qsum`` will decrease to zero
     immediately when frequency/voltage protection is triggered. Once the lock is
     released, ``Psum`` and ``Qsum`` will return to normal immediately.
 
@@ -407,23 +419,22 @@ class PLK2(PLK2Data, PLK2Model):
 
     The model does not check the shedding points sort.
     The input data is required to satisfy `fl3 < fl2 < fl1 < fu1 < fu2 < fu3`, and
-    `ul4 < ul3 < ul2 < ul1 < `.
+    `ul4 < ul3 < ul2 < ul1 < uu1 < uu2 < uu3`.
 
     Frequency (Hz):\n
-    (fl3, fl2), Tfl2; [(50.0, 57.5), 10s]\n
-    (fl2, fl1), Tfl1; [(57.5, 59.2), 300s]\n
-    (fu1, fu2), Tfu1; [(60.5, 61.5), 300s]\n
-    (fu2, fu3), Tfu2; [(61.5, 70.0), 10s]\n
+    `(fl3, fl2), Tfl2`; [(50.0, 57.5), 10s]\n
+    `(fl2, fl1), Tfl1`; [(57.5, 59.2), 300s]\n
+    `(fu1, fu2), Tfu1`; [(60.5, 61.5), 300s]\n
+    `(fu2, fu3), Tfu2`; [(61.5, 70.0), 10s]\n
 
     Voltage (p.u.):\n
-    (vl4, vl3), Tvl3; [(0.10, 0.45), 0.16s]\n
-    (vl3, vl2), Tvl2; [(0.45, 0.60), 1s]\n
-    (vl2, vl1), Tvl1; [(0.60, 0.88), 2s]\n
-    (vu1, vu2), Tvu1; [(1.10, 1.20), 1s]\n
-    (vu2, vu3), Tvu2; [(1.20, 2.00), 0.16s]\n
+    `(vl4, vl3), Tvl3`; [(0.10, 0.45), 0.16s]\n
+    `(vl3, vl2), Tvl2`; [(0.45, 0.60), 1s]\n
+    `(vl2, vl1), Tvl1`; [(0.60, 0.88), 2s]\n
+    `(vu1, vu2), Tvu1`; [(1.10, 1.20), 1s]\n
+    `(vu2, vu3), Tvu2`; [(1.20, 2.00), 0.16s]\n
     """
 
     def __init__(self, system, config):
         PLK2Data.__init__(self)
         PLK2Model.__init__(self, system, config)
-
