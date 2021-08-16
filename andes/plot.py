c02ebb612974d8591d4d5096be4f53e97263ae9c
Hantao Cui <cuihantao@gmail.com>
2021-08-16 16:37:46 -0500

    Added `mask` option to only use data for the specified `left` and `right`

--- andes/plot.py
+++ andes/plot.py
@@ -35,10 +35,10 @@ class TDSData:
         self._lst_file = None
 
         # data members for raw data
-        self._idx = []  # indices of variables
+        self._idx = []    # indices of variables
         self._uname = []  # unformatted variable names
         self._fname = []  # formatted variable names
-        self._data = []  # data loaded from file
+        self._data = []   # data loaded from file
 
         # auxillary data members for fast query
         self.t = []
@@ -272,7 +272,8 @@ class TDSData:
              title=None, linestyles=None, use_bqplot=False,
              hline1=None, hline2=None, vline1=None, vline2=None,
              fig=None, ax=None, backend=None,
-             set_xlim=True, set_ylim=True, autoscale=False, legend_bbox=None, legend_loc=None,
+             set_xlim=True, set_ylim=True, autoscale=False,
+             legend_bbox=None, legend_loc=None, legend_ncol=1,
              figsize=None,
              **kwargs):
         """
@@ -321,6 +322,10 @@ class TDSData:
             A list containing the variable names for the x-axis variable
         legend : bool
             True to show legend and False otherwise
+        legend_ncol : int
+            Number of columns in legend
+        legend_bbox : tuple of two floats
+            legend box to anchor
         grid : bool
             True to show grid and False otherwise
         latex : bool
@@ -408,7 +413,8 @@ class TDSData:
                          hline1=hline1, hline2=hline2, vline1=vline1, vline2=vline2,
                          fig=fig, ax=ax, linestyles=linestyles,
                          set_xlim=set_xlim, set_ylim=set_ylim, autoscale=autoscale,
-                         legend_bbox=legend_bbox, legend_loc=legend_loc, figsize=figsize,
+                         legend_bbox=legend_bbox, legend_loc=legend_loc, legend_ncol=legend_ncol,
+                         figsize=figsize,
                          **kwargs)
 
     def get_call(self, backend=None):
@@ -467,7 +473,8 @@ class TDSData:
                   latex=True, dpi=DPI, line_width=1.0, font_size=12, greyscale=False, savefig=None,
                   save_format=None, show=True, title=None, hline1=None, hline2=None, vline1=None,
                   vline2=None, set_xlim=True, set_ylim=True, autoscale=False, figsize=None,
-                  legend_bbox=None, legend_loc=None,
+                  legend_bbox=None, legend_loc=None, legend_ncol=1,
+                  mask=True,
                   **kwargs):
         """
         Plot lines for the supplied data and options.
@@ -484,6 +491,10 @@ class TDSData:
         ydata : array
             An array containing the values of each variables for the y-axis variable. The row
             of `ydata` must match the row of `xdata`. Each column correspondings to a variable.
+        mask : bool
+            If enabled (1), when specifying axis limits, only data in the limits will be
+            used for plotting to optimize for autoscaling.
+            It is done through an index mask.
 
         Returns
         -------
@@ -535,6 +546,11 @@ class TDSData:
         if greyscale:
             plt.gray()
 
+        if mask is True:
+            mask = (xdata >= (left - 0.1)) & (xdata <= (right + 0.1))
+            xdata = xdata[mask]
+            ydata = ydata[mask.reshape(-1, )]
+
         for i in range(n_lines):
             ax.plot(xdata,
                     ydata[:, i],
@@ -571,7 +587,9 @@ class TDSData:
                 legend = True
 
         if legend:
-            ax.legend(bbox_to_anchor=legend_bbox, loc=legend_loc)
+            ax.legend(bbox_to_anchor=legend_bbox,
+                      loc=legend_loc,
+                      ncol=legend_ncol)
 
         if title:
             ax.set_title(title)
