21a081e72f34f5deeea1e96ef66b49e5801fb782
Jinning Wang <jwang175@CEPD00648.curent.utk.edu>
2021-08-24 10:00:50 -0400

    Fixed ``Qsum`` lock.

--- andes/models/distributed/plk2.py
+++ andes/models/distributed/plk2.py
@@ -16,9 +16,11 @@ class PLK2Data(ModelData):
 
     def __init__(self):
         super(PLK2Data, self).__init__()
+
         self.dev = IdxParam(info='idx of the target device',
                             mandatory=True,
                             )
+
         self.busfreq = IdxParam(model='BusFreq',
                                 info='Target device interface bus measurement device idx',
                                 )
@@ -30,6 +32,7 @@ class PLK2Data(ModelData):
                             info='Frequency deviation protection enable. \
                                   1 for enable, 0 for disable.',
                             )
+
         self.Ven = NumParam(default=0,
                             tex_name='Vena',
                             vrange=[0, 1],
@@ -48,26 +51,31 @@ class PLK2Data(ModelData):
                             info='Under frequency shadding point 3',
                             unit='Hz',
                             )
+
         self.fl2 = NumParam(default=57.5,
                             tex_name='fl2',
                             info='Over frequency shadding point 2',
                             unit='Hz',
                             )
+
         self.fl1 = NumParam(default=59.2,
                             tex_name='fl1',
                             info='Under frequency shadding point 1',
                             unit='Hz',
                             )
+
         self.fu1 = NumParam(default=60.5,
                             tex_name='fu1',
                             info='Over frequency shadding point 1',
                             unit='Hz',
                             )
+
         self.fu2 = NumParam(default=61.5,
                             tex_name='fu2',
                             info='Over frequency shadding point 2',
                             unit='Hz',
                             )
+
         self.fu3 = NumParam(default=70,
                             tex_name='fu3',
                             info='Over frequency shadding point 3',
@@ -79,16 +87,19 @@ class PLK2Data(ModelData):
                              info='Stand time for (fl2, fl1)',
                              non_negative=True,
                              )
+
         self.Tfl2 = NumParam(default=10,
                              tex_name=r't_{fl2}',
                              info='Stand time for (fl3, fl2)',
                              non_negative=True,
                              )
+
         self.Tfu1 = NumParam(default=300,
                              tex_name=r't_{tu1}',
                              info='Stand time for (fu1, fu2)',
                              non_negative=True,
                              )
+
         self.Tfu2 = NumParam(default=10,
                              tex_name=r't_{fu2}',
                              info='Stand time for (fu2, fu3)',
@@ -101,31 +112,37 @@ class PLK2Data(ModelData):
                             info='Under voltage shadding point 4',
                             unit='p.u.',
                             )
+
         self.vl3 = NumParam(default=0.45,
                             tex_name='vl3',
                             info='Under voltage shadding point 3',
                             unit='p.u.',
                             )
+
         self.vl2 = NumParam(default=0.6,
                             tex_name='vl2',
                             info='Under voltage shadding point 2',
                             unit='p.u.',
                             )
+
         self.vl1 = NumParam(default=0.88,
                             tex_name='vl1',
                             info='Under voltage shadding point 1',
                             unit='p.u.',
                             )
+
         self.vu1 = NumParam(default=1.1,
                             tex_name='vu1',
                             info='Over voltage shadding point 1',
                             unit='p.u.',
                             )
+
         self.vu2 = NumParam(default=1.2,
                             tex_name='vu2',
                             info='Over voltage shadding point 2',
                             unit='p.u.',
                             )
+
         self.vu3 = NumParam(default=2,
                             tex_name='vu3',
                             info='Over voltage shadding point 3',
@@ -137,21 +154,25 @@ class PLK2Data(ModelData):
                              info='Stand time for (vl2, vl1)',
                              non_negative=True,
                              )
+
         self.Tvl2 = NumParam(default=1,
                              tex_name=r't_{vl2}',
                              info='Stand time for (vl3, vl2)',
                              non_negative=True,
                              )
+
         self.Tvl3 = NumParam(default=0.16,
                              tex_name=r't_{vl3}',
                              info='Stand time for (vl4, vl3)',
                              non_negative=True,
                              )
+
         self.Tvu1 = NumParam(default=1,
                              tex_name=r't_{vu1}',
                              info='Stand time for (vu1, vu2)',
                              non_negative=True,
                              )
+
         self.Tvu2 = NumParam(default=0.16,
                              tex_name=r't_{vu2}',
                              info='Stand time for (vu2, vu3)',
@@ -172,6 +193,7 @@ class PLK2Model(Model):
         self.bus = ExtParam(model='DG', src='bus',
                             indexer=self.dev,
                             export=False)
+
         self.fn = ExtParam(model='DG', src='fn',
                            indexer=self.dev,
                            export=False)
@@ -183,6 +205,7 @@ class PLK2Model(Model):
                           export=False,
                           info='Bus frequency', unit='p.u.',
                           )
+
         self.fHz = Algeb(v_str='fn * f',
                          e_str='fn * f - fHz',
                          info='frequency in Hz',
@@ -195,16 +218,19 @@ class PLK2Model(Model):
                             info='Frequency comparer for (fl3, fl1)',
                             equal=False, no_warn=True,
                             )
+
         self.Lfl2 = Limiter(u=self.fHz,
                             lower=self.fl3, upper=self.fl2,
                             info='Frequency comparer for (fl3, fl2)',
                             equal=False, no_warn=True,
                             )
+
         self.Lfu1 = Limiter(u=self.fHz,
                             lower=self.fu1, upper=self.fu3,
                             info='Frequency comparer for (fu1, fu3)',
                             equal=False, no_warn=True,
                             )
+
         self.Lfu2 = Limiter(u=self.fHz,
                             lower=self.fu2, upper=self.fu3,
                             info='Frequency comparer for (fu2, fu3)',
@@ -217,6 +243,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (fl3, fl1)',
                                  )
+
         self.FLfl1 = Limiter(u=self.INTfl1_y,
                              lower=0, upper=self.Tfl1,
                              info='Flag comparer for (fl3, fl1)',
@@ -228,6 +255,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (fl3, fl2)',
                                  )
+
         self.FLfl2 = Limiter(u=self.INTfl2_y,
                              lower=0, upper=self.Tfl2,
                              info='Flag comparer for (fl3, fl2)',
@@ -239,6 +267,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (fu1, fu3)',
                                  )
+
         self.FLfu1 = Limiter(u=self.INTfu1_y,
                              lower=0, upper=self.Tfu1,
                              info='Flag comparer for (fu1, fu3)',
@@ -250,6 +279,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (fu2, fu3)',
                                  )
+
         self.FLfu2 = Limiter(u=self.INTfu2_y,
                              lower=0, upper=self.Tfu2,
                              info='Flag comparer for (fu2, fu3)',
@@ -263,27 +293,32 @@ class PLK2Model(Model):
                           info='Bus voltage',
                           unit='p.u.',
                           )
+
         # Indicatior of voltage deviation
         self.LVl1 = Limiter(u=self.v,
                             lower=self.vl4, upper=self.vl1,
                             info='Voltage comparer for (vl4, vl1)',
                             equal=False, no_warn=True,
                             )
+
         self.LVl2 = Limiter(u=self.v,
                             lower=self.vl4, upper=self.vl2,
                             info='Voltage comparer for (vl4, vl2)',
                             equal=False, no_warn=True,
                             )
+
         self.LVl3 = Limiter(u=self.v,
                             lower=self.vl4, upper=self.vl3,
                             info='Voltage comparer for (vl4, vl3)',
                             equal=False, no_warn=True,
                             )
+
         self.LVu1 = Limiter(u=self.v,
                             lower=self.vu1, upper=self.vu3,
                             info='Voltage comparer for (vu1, vu3)',
                             equal=False, no_warn=True,
                             )
+
         self.LVu2 = Limiter(u=self.v,
                             lower=self.vu2, upper=self.vu3,
                             info='Voltage comparer for (vu2, vu3)',
@@ -296,6 +331,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (vl4, vl1)',
                                  )
+
         self.FLVl1 = Limiter(u=self.INTVl1_y,
                              lower=0, upper=self.Tvl1,
                              info='Flag comparer for (vl4, vl1)',
@@ -307,6 +343,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (vl4, vl2)',
                                  )
+
         self.FLVl2 = Limiter(u=self.INTVl2_y,
                              lower=0, upper=self.Tvl2,
                              info='Flag comparer for (vl4, vl2)',
@@ -319,6 +356,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (vl4, vl3)',
                                  )
+
         self.FLVl3 = Limiter(u=self.INTVl3_y,
                              lower=0, upper=self.Tvl3,
                              info='Flag comparer for (vl4, vl3)',
@@ -331,6 +369,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (vu1, vu3)',
                                  )
+
         self.FLVu1 = Limiter(u=self.INTVu1_y,
                              lower=0, upper=self.Tvu1,
                              info='Flag comparer for (vu1, vu3)',
@@ -342,6 +381,7 @@ class PLK2Model(Model):
                                  y0='0',
                                  info='Flag integerator for (vu2, vu3)',
                                  )
+
         self.FLVu2 = Limiter(u=self.INTVu2_y,
                              lower=0, upper=self.Tvu2,
                              info='Flag comparer for (vu2, vu3)',
@@ -354,32 +394,40 @@ class PLK2Model(Model):
         self.ltl = ConstService(v_str='0.2')
 
         # `Ldsum_zu` is `ue`
-        dsum = 'fen * (FLfl1_zu + FLfl2_zu + FLfu1_zu + FLfu2_zu) + \
-                Ven * (FLVl1_zu + FLVl2_zu + FLVl3_zu + FLVu1_zu + \
-                       FLVu2_zu) - \
-                dsum'
+        dsum = 'fen * (FLfl1_zu + FLfl2_zu + FLfu1_zu + FLfu2_zu) + ' \
+               'Ven * (FLVl1_zu + FLVl2_zu + FLVl3_zu + \
+                       FLVu1_zu + FLVu2_zu) - ' \
+               'dsum'
+
         self.dsum = Algeb(v_str='0',
                           e_str=dsum,
-                          info='Protection signal summation',
+                          info='lock signal summation',
                           tex_name=r'd_{sum}',
                           )
+
         self.Ldsum = Limiter(u=self.dsum,
                              lower=self.ltl,
                              upper=self.ltu,
-                             info='Protection signal comparer, zu is to act',
+                             info='lock signal comparer, zu is to act',
+                             equal=False, no_warn=True,
                              )
 
-        # self.ue = AliasAlgeb(self.Ldsum_zu)
+        self.ue = Algeb(v_str='0',
+                        e_str='Ldsum_zu - ue',
+                        info='lock flag',
+                        tex_name=r'ue',
+                        )
 
         # lock freq signal of BusFreq
         self.WOy = ExtAlgeb(model='FreqMeasurement', src='WO_y',
                             indexer=self.busfreq,
                             info='original Washout y from BusFreq',
                             )
+
         self.fmea = ExtAlgeb(model='FreqMeasurement', src='f',
                              indexer=self.busfreq,
                              export=False,
-                             e_str='-Ldsum_zu * (1 + WOy)',
+                             e_str='- ue * (1 + WOy)',
                              info='Active power lock',
                              )
 
@@ -388,34 +436,41 @@ class PLK2Model(Model):
                              indexer=self.dev,
                              info='original Pext from DG',
                              )
+
         self.Pref = ExtAlgeb(model='DG', src='Pref',
                              indexer=self.dev,
                              info='original Pref from DG',
                              )
+
         self.Pdrp = ExtAlgeb(model='DG', src='DB_y',
                              indexer=self.dev,
                              info='original Pdrp from DG',
                              )
+
         self.Psum = ExtAlgeb(model='DG', src='Psum',
                              indexer=self.dev,
                              export=False,
-                             e_str='-Ldsum_zu * (Pext + Pref + Pdrp)',
+                             e_str='- ue * (Pext + Pref + Pdrp)',
                              info='Active power lock',
                              )
 
-        # TODO: rewrite Qsum eqn of PVD1 model
-        # TODO: fix Qsum lock
-        # self.Qorg = ExtAlgeb(model='DG', src='DB_y',
-        #                      indexer=self.dev,
-        #                      info='original Qsum from DG',
-        #                      )
-        self.Qsum = ExtAlgeb(model='DG',
-                             src='Qsum',
+        self.Qdrp = ExtAlgeb(model='DG', src='Qdrp',
+                             indexer=self.dev,
+                             info='original Qdrp from DG',
+                             )
+
+        self.Qref = ExtAlgeb(model='DG', src='Qref',
+                             indexer=self.dev,
+                             info='original Qref from DG',
+                             )
+
+        self.Qsum = ExtAlgeb(model='DG', src='Qsum',
                              indexer=self.dev,
                              export=False,
-                             e_str='-Ldsum_zu * 10 * Qsum',
+                             e_str='- ue * (Qdrp + Qref)',
                              info='Reactive power lock',
                              )
+
         # TODO: clear State: BusFreq L(Lag), BusFreq WO(Washout)
         # TODO: clear State: PVD1: Ipout, Iqout
 
