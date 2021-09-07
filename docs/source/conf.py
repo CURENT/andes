45985cb2f702a5cf7587567e24db7b120176f941
Hantao Cui <cuihantao@gmail.com>
2021-09-07 10:47:38 -0500

    Removed `myst` as a dependency.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,6 @@ extensions = [
     'IPython.sphinxext.ipython_console_highlighting',
     'matplotlib.sphinxext.plot_directive',
     'numpydoc',
-    'myst_parser',
     'sphinx_copybutton',
 ]
 
