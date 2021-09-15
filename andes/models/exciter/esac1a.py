0445669a6e459ac210ffbed2156273be0ce0d81b
Jinning Wang <jwang175@CEPD00648.curent.utk.edu>
2021-09-14 22:47:49 -0400

    Added Washout feedback to input into `ESAC1A`.

--- andes/models/exciter/esac1a.py
+++ andes/models/exciter/esac1a.py
@@ -4,7 +4,7 @@ from andes.core.var import Algeb
 from andes.core.service import ConstService, VarService, FlagValue
 
 from andes.core.block import LagAntiWindup, LeadLag, Washout, Lag, HVGate
-from andes.core.block import LessThan
+from andes.core.block import LessThan, LVGate, AntiWindup, IntegratorAntiWindup
 from andes.core.block import Integrator
 
 from andes.models.exciter.excbase import ExcBase, ExcBaseData
@@ -38,6 +38,39 @@ class ESAC1AData(ExcBaseData):
                            default=0.04,
                            unit='p.u.',
                            )
+        self.VRMAX = NumParam(info='Max. exc. limit (0-unlimited)',
+                              tex_name='V_{RMAX}',
+                              default=7.3,
+                              unit='p.u.')
+        self.VRMIN = NumParam(info='Min. excitation limit',
+                              tex_name='V_{RMIN}',
+                              default=-7.3,
+                              unit='p.u.')
+        self.TE = NumParam(info='Integrator time constant',
+                           tex_name='T_E',
+                           default=0.8,
+                           unit='p.u.',
+                           )
+        self.KF = NumParam(default=0.1,
+                           info='Feedback gain',
+                           tex_name='K_F',
+                           )
+        self.TF = NumParam(info='Feedback washout time constant',
+                           tex_name='T_{F1}',
+                           default=1,
+                           unit='p.u.',
+                           non_negative=True,
+                           non_zero=True,
+                           )
+
+        self.Switch = NumParam(info='Switch that PSS/E did not implement',
+                               tex_name='S_w',
+                               default=0,
+                               unit='bool',
+                               )
+
+
+
 
 
 
@@ -50,15 +83,14 @@ class ESAC1AModel(ExcBase):
                       info='Voltage transducer',
                       )
 
-        # TODO: Question: where is the limiter in ESDC2A?
-        # TODO: Question: the sequence of HG and LL in ESDC2A?
-        # TODO: add vref
+        # TODO: add self.vref
 
+        # TODO: modify v_str for VF
         # input excitation voltages; PSS outputs summed at vi
         self.vi = Algeb(info='Total input voltages',
                         tex_name='V_i',
                         unit='p.u.',
-                        e_str='-LG_y + vref - vi',
+                        e_str='-LG_y + vref + VF - vi',
                         v_str='-v + vref',
                         )
 
@@ -76,6 +108,63 @@ class ESAC1AModel(ExcBase):
                                 lower=self.VRL,
                                 info='Anti-windup lag',
                                 )  # LA_y == VR
+
+        # TODO: check if eqn is correct
+        self.UEL = Algeb(info='Interface var for under exc. limiter',
+                         tex_name='U_{EL}',
+                         v_str='0',
+                         e_str='0 - UEL'
+                         )
+
+        # TODO: check info
+        self.HG = HVGate(u1=self.UEL,
+                         u2=self.LA_y,
+                         info='HVGate for under excitation',
+                         )
+
+        # TODO: check if eqn is correct, seems wrong
+        self.OEL = Algeb(info='Interface var for under exc. limiter',
+                         tex_name='O_{EL}',
+                         v_str='0',
+                         e_str='0 - OEL'
+                         )
+        # TODO: check info
+        self.LG = LVGate(u1=self.HG_y,
+                         u2=self.OEL,
+                         info='LVGate for under excitation',
+                         )
+
+        self.AW = AntiWindup(u=self.LG_y,
+                             lower=self.VRMIN,
+                             upper=self.VRMAX,
+                             tex_name=r'lim_{LG}',
+                             )
+
+        # TODO: check y0
+        # TODO: add self.vf0
+        self.large = ConstService('999')
+        self.IAW = IntegratorAntiWindup(u='ue * (AW_y - VFE)',
+                                        T=self.TE,
+                                        K=1,
+                                        y0=self.vf0,
+                                        lower=self.zero,
+                                        upper=self.large,
+                                        info='IntegratorAntiWindup,
+                                        )
+
+        # TODO: add VFE
+
+        self.VF = Washout(u=self.INT_y,
+                          T=self.TF,
+                          K=self.KF,
+                          info='Feedback to input'
+                          )
+
+        # TODO: add saturation VX
+
+        # TODO: add FEX
+
+
 
 
 
