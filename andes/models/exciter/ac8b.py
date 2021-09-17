348870532b56c71e1722301fd5976126f07200b7
Jinning Wang <jinningwang@jwang175mba.nomads.utk.edu>
2021-09-17 13:29:46 -0400

    Added PID.

--- andes/models/exciter/ac8b.py
+++ andes/models/exciter/ac8b.py
@@ -5,7 +5,7 @@ from andes.core.service import ConstService, VarService, FlagValue
 
 from andes.core.block import LagAntiWindup, LeadLag, Washout, Lag, HVGate
 from andes.core.block import LessThan, LVGate, AntiWindup, IntegratorAntiWindup
-from andes.core.block import Integrator
+from andes.core.block import Integrator, PIDAWHardLimit
 
 from andes.models.exciter.excbase import ExcBase, ExcBaseData
 from andes.models.exciter.saturation import ExcQuadSat
@@ -24,7 +24,12 @@ class AC8BModel(ExcBase):
     def __init__(self, system, config):
         ExcBase.__init__(self, system, config)
 
-
+        self.PIDAW = PIDAWHardLimit(u=self.vin, kp=self.Kp, ki=self.Ki, kd=self.Kd,
+                                    aw_lower=self.thmin, aw_upper=self.thmax,
+                                    lower=self.thmin, upper=self.thmax,
+                                    tex_name='PID',
+                                    info='PID',
+                                    )
 
 
 class AC8B(AC8BData, AC8BModel):
@@ -43,7 +48,6 @@ class AC8B(AC8BData, AC8BModel):
 
     https://www.neplan.ch/wp-content/uploads/2015/08/Nep_EXCITERS1.pdf
     """
-
     def __init__(self, system, config):
         AC8BData.__init__(self)
         AC8BModel.__init__(self, system, config)
