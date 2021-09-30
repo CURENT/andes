cb825663d19a7b81a0cd6500d7a5d744b1a7e67a
Jinning Wang <jinningwang@jwang175mba.nomads.utk.edu>
2021-09-29 20:04:03 -0400

    ESST1A in process.

--- andes/models/exciter/esst1a.py
+++ andes/models/exciter/esst1a.py
@@ -14,7 +14,20 @@ class ESST1AData(ExcBaseData):
     def __init__(self):
         ExcBaseData.__init__(self)
 
+        self.KA = NumParam(default=80,
+                           info='Regulator gain',
+                           tex_name='K_A',
+                           )
+        self.TA = NumParam(info='Lag time constant in regulator',
+                           tex_name='T_A',
+                           default=0.04,
+                           unit='p.u.',
+                           )
 
+        self.KLR = NumParam(default=1,
+                            info='Exciter output current limiter gain',
+                            tex_name=r'K_LR',
+                            )
 
         self.KF = NumParam(default=0.1,
                            info='Feedback gain',
@@ -36,14 +49,51 @@ class ESST1AModel(ExcBase):
         ExcVsum.__init__(self)
 
 
+
+
+        self.LL = LeadLag(u=self.vi,
+                          T1=self.TC,
+                          T2=self.TB,
+                          info='Lead-lag compensator',
+                          zero_out=True,
+                          )
+
+        self.LL1 = LeadLag(u=self.LL_y,
+                           T1=self.TC1,
+                           T2=self.TB1,
+                           info='Lead-lag compensator 1',
+                           zero_out=True,
+                           )
+
+
+        self.LA = LagAntiWindup(u=self.LL_y,
+                                T=self.TA,
+                                K=self.KA,
+                                upper=self.VAMAX,
+                                lower=self.VAMIN,
+                                info='V_A, Anti-windup lag',
+                                )  # LA_y == VR
+
+        # TODO: check values
+        self.UEL0.v_str = '-999'
+        self.OEL0.v_str = '999'
+
         self.vin = Algeb(info='input voltage',
                          tex_name='v_{in}',
                          v_str='VFE',
                          e_str='ue * (1-LVC_zl) * OEL + LVC_zl * HVG_y - vin',
                          )
 
+        self.ILR0 = ConstService(v_str='0', tex_name='I_{LR0}', info='ILR initial value')
+
+        self.ILR = Algeb(info='exciter output current limit reference',
+                         tex_name='I_{LR}}',
+                         v_str='VFE',
+                         e_str='ILR0 - ILR',
+                         )
+
         self.HVG = HVGate(u1=self.UEL,
-                          u2=self.LA_y,
+                          u2='LA_y - KLR * (ILR - XadIfd)',
                           info='HVGate for under excitation',
                           )
 
