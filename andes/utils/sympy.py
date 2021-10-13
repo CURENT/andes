70d330cd29c088beed7b03bb41ea08f0e13d40ab
Hantao Cui <cuihantao@gmail.com>
2021-10-13 17:55:44 -0500

    Renamed first argument.

--- andes/utils/sympy.py
+++ andes/utils/sympy.py
@@ -8,13 +8,13 @@ class fixPiecewise(sym.Piecewise):
     See: https://github.com/sympy/sympy/issues/15014
     """
 
-    def _numpycode(expr, printer):
+    def _numpycode(self, printer):
         """
         Updated numpy code printer.
         """
 
         # find any symbol:
-        s = list(expr.atoms(sym.Symbol))[0]
+        s = list(self.atoms(sym.Symbol))[0]
 
         def broadcastarg(arg):
             if arg.has(sym.Symbol):
@@ -31,11 +31,11 @@ class fixPiecewise(sym.Piecewise):
                 return printer._module+'.full({0}.shape,{1})'.format(printer._print(s), printer._print(cond))
 
         "Piecewise function printer"
-        exprs = '[{}]'.format(','.join(broadcastarg(arg.expr) for arg in expr.args))
-        conds = '[{}]'.format(','.join(broadcastcond(arg.cond) for arg in expr.args))
+        exprs = '[{}]'.format(','.join(broadcastarg(arg.expr) for arg in self.args))
+        conds = '[{}]'.format(','.join(broadcastcond(arg.cond) for arg in self.args))
         # If [default_value, True] is a (expr, cond) sequence in a Piecewise object
         #     it will behave the same as passing the 'default' kwarg to select()
-        #     *as long as* it is the last element in expr.args.
+        #     *as long as* it is the last element in self.args.
         # If this is not the case, it may be triggered prematurely.
         return '{}({}, {}, default={})'.format(
             printer._module_format(printer._module + '.select'), conds, exprs,
