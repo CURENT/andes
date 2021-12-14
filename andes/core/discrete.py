c5aa431d8c2208d657c568c33c803f6cc77f82b4
Hantao Cui <cuihantao@gmail.com>
2021-12-13 20:37:45 -0600

    Enter show_adjust only during init.

--- andes/core/discrete.py
+++ andes/core/discrete.py
@@ -701,17 +701,21 @@ class AntiWindup(Limiter):
         """
         if not self.no_upper:
             upper_v = -self.upper.v if self.sign_upper.v == -1 else self.upper.v
-            self.do_adjust_upper(self.u.v, upper_v,
-                                 allow_adjust=allow_adjust,
-                                 adjust_upper=adjust_upper)
+
+            if self.allow_adjust and allow_adjust and adjust_upper:
+                self.do_adjust_upper(self.u.v, upper_v,
+                                    allow_adjust=allow_adjust,
+                                    adjust_upper=adjust_upper)
             self.zu[:] = np.logical_and(np.greater_equal(self.u.v, upper_v),
                                         np.greater_equal(self.state.e, 0))
 
         if not self.no_lower:
             lower_v = -self.lower.v if self.sign_lower.v == -1 else self.lower.v
-            self.do_adjust_lower(self.u.v, lower_v,
-                                 allow_adjust=allow_adjust,
-                                 adjust_lower=adjust_lower)
+
+            if self.allow_adjust and allow_adjust and adjust_lower:
+                self.do_adjust_lower(self.u.v, lower_v,
+                                    allow_adjust=allow_adjust,
+                                    adjust_lower=adjust_lower)
             self.zl[:] = np.logical_and(np.less_equal(self.u.v, lower_v),
                                         np.less_equal(self.state.e, 0))
 
