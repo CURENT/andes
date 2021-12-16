a6129ca0184c256c5228c00024c4f13c7803c5ef
Jinning Wang <jinningwang@jwang175mba.nomads.utk.edu>
2021-12-16 14:24:39 -0500

    Developed HYGOV and HYGOVDB.

--- andes/models/governor/__init__.py
+++ andes/models/governor/__init__.py
@@ -4,3 +4,4 @@ from andes.models.governor.ieesgo import IEESGO  # NOQA
 from andes.models.governor.tg2 import TG2  # NOQA
 from andes.models.governor.tgov1 import TGOV1NDB  # NOQA
 from andes.models.governor.tgov1 import TGOV1, TGOV1DB, TGOV1N  # NOQA
+from andes.models.governor.hygov import HYGOV, HYGOVDB  # NOQA
