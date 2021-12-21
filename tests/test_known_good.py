c8b82ff325b0cb9cec28fc0aa03fb81d82901d11
Hantao Cui <cuihantao@gmail.com>
2021-12-21 10:53:41 -0600

    Another attempt to fix github CI.

--- tests/test_known_good.py
+++ tests/test_known_good.py
@@ -54,7 +54,6 @@ def compare_results(case, pkl_name, addfile=None, tf=10):
     ss.TDS.config.tol = 1e-4
     ss.TDS.config.fixt = 1
     ss.TDS.config.shrinkt = 0
-    ss.TDS.config.honest = 0
     ss.TDS.run()
 
     test_dir = os.path.dirname(__file__)
