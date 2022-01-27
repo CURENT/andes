3a7a72903569ce0b51504362cd07409ffdb7c936
Hantao <cuihantao@gmail.com>
2022-01-27 15:16:06 -0600

    Let tqdm auto set width.

--- andes/routines/tds.py
+++ andes/routines/tds.py
@@ -321,7 +321,7 @@ class TDS(BaseRoutine):
             logger.debug("Initialization only is requested and done")
             return self.initialized
 
-        self.pbar = tqdm(total=100, ncols=70, unit='%', ascii=True,
+        self.pbar = tqdm(total=100, unit='%', ascii=True,
                          file=sys.stdout, disable=self.config.no_tqdm)
 
         if resume:
