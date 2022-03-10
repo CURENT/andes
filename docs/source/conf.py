139a0852bb9a229203523b0a7f751bb6d6df3dae
Hantao <cuihantao@gmail.com>
2022-03-10 14:24:13 -0600

    Added sympy's intersphinx.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -203,6 +203,7 @@ intersphinx_mapping = {
     'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable', None),
     'matplotlib': ('https://matplotlib.org', None),
+    'sympy': ('https://docs.sympy.org/latest/', None),
 }
 
 # Favorite icon
