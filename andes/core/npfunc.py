cfa8c3bae5d057463de44e3bae3636959e50e050
Hantao <cuihantao@gmail.com>
2022-03-10 14:23:46 -0600

    Moved inherited sympy class to separate folder to pass doc build

--- andes/core/npfunc.py
+++ andes/core/npfunc.py
@@ -6,11 +6,13 @@ def safe_div(a, b, out=None):
     Safe division for numpy. Division by zero yields zero.
 
     `safe_div` cannot be used in `e_str` due to unsupported Derivative.
+
     Parameters
     ----------
     out : None or array-like
         If not None, out contains the default values for where b==0.
     """
+
     if out is None:
         out = np.zeros_like(a)
 
