07929dc3475fd48571f855469daf18c63a9d12ea
jinningwang <jinninggm@gmail.com>
2022-03-25 13:02:09 -0400

    Add test for make_link_table

--- tests/test_pandapower.py
+++ tests/test_pandapower.py
@@ -42,11 +42,18 @@ class TestPandapower(unittest.TestCase):
         self.v_pp = ssp.res_bus['vm_pu']
         self.a_pp = ssp.res_bus['va_degree']
 
+        self.link_table = make_link_table(ssa)
+
     def test_to_pandapower(self):
 
         np.testing.assert_almost_equal(self.v_andes, self.v_pp, decimal=6)
         np.testing.assert_almost_equal(self.a_andes, self.a_pp, decimal=6)
 
     def test_make_link_table(self):
-        pass
+        ridx = self.link_table[self.link_table['syn_idx'] == 'GENROU_1'].index
+        c_bus = self.link_table['bus_name'].iloc[ridx].astype(str) == 'BUS1'
+        c_exc = self.link_table['exc_idx'].iloc[ridx].astype(str) == 'ESST3A_2'
+        c_stg = self.link_table['stg_idx'].iloc[ridx].astype(str) == '1'
+        c_gov = self.link_table['gov_idx'].iloc[ridx].astype(str) == 'TGOV1_1'
+        return c_bus[0] and c_exc[0] and c_stg[0] and c_gov[0]
         # make_link_table
