a4fbe3f82c221ccdd075d4aa041600fc51711043
jinningwang <jinninggm@gmail.com>
2022-03-25 12:31:43 -0400

    Add test for to_pandapower

--- tests/test_pandapower.py
+++ tests/test_pandapower.py
@@ -3,6 +3,10 @@ Test ANDES-pandapower interface.
 """
 
 import unittest
+import numpy as np
+from andes.interop.pandapower import to_pandapower, make_link_table
+from math import pi
+import andes
 
 
 try:
@@ -22,7 +26,27 @@ class TestPandapower(unittest.TestCase):
         """
         Test setup. This is executed before each test case.
         """
-        pass
+
+        ssa = andes.load(andes.get_case('ieee14/ieee14_ieeet1.xlsx'),
+                         setup=True,
+                         no_output=True,
+                         default_config=True)
+        ssp = to_pandapower(ssa)
+
+        ssa.PFlow.run()
+        pp.runpp(ssp)
+
+        self.v_andes = ssa.Bus.v.v
+        self.a_andes = ssa.Bus.a.v * 180 / pi
+
+        self.v_pp = ssp.res_bus['vm_pu']
+        self.a_pp = ssp.res_bus['va_degree']
 
     def test_to_pandapower(self):
+
+        np.testing.assert_almost_equal(self.v_andes, self.v_pp, decimal=6)
+        np.testing.assert_almost_equal(self.a_andes, self.a_pp, decimal=6)
+
+    def test_make_link_table(self):
         pass
+        # make_link_table
