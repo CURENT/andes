dfd9aca7237abd4c4d4408c7e766dfda6d826e3d
jinningwang <jinninggm@gmail.com>
2022-03-25 13:04:35 -0400

    Format test_pandapower

--- tests/test_pandapower.py
+++ tests/test_pandapower.py
@@ -45,15 +45,19 @@ class TestPandapower(unittest.TestCase):
         self.link_table = make_link_table(ssa)
 
     def test_to_pandapower(self):
-
+        """
+        Test `andes.interop.pandapower.to_pandapower`
+        """
         np.testing.assert_almost_equal(self.v_andes, self.v_pp, decimal=6)
         np.testing.assert_almost_equal(self.a_andes, self.a_pp, decimal=6)
 
     def test_make_link_table(self):
+        """
+        Test `andes.interop.pandapower.make_link_table`
+        """
         ridx = self.link_table[self.link_table['syn_idx'] == 'GENROU_1'].index
         c_bus = self.link_table['bus_name'].iloc[ridx].astype(str) == 'BUS1'
         c_exc = self.link_table['exc_idx'].iloc[ridx].astype(str) == 'ESST3A_2'
         c_stg = self.link_table['stg_idx'].iloc[ridx].astype(str) == '1'
         c_gov = self.link_table['gov_idx'].iloc[ridx].astype(str) == 'TGOV1_1'
         return c_bus[0] and c_exc[0] and c_stg[0] and c_gov[0]
-        # make_link_table
