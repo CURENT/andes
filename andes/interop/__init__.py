b26460a295788d22d90e3679de56e7414ea95e8d
Hantao Cui <cuihantao@gmail.com>
2022-03-26 12:48:37 -0500

    Add notes for installation.

--- andes/interop/__init__.py
+++ andes/interop/__init__.py
@@ -1,3 +1,16 @@
 """
 Interopability package between Andes and other software.
+
+To install dependencies, do:
+
+.. code:: bash
+
+    pip install andes[interop]
+
+To install dependencies for *development*, in the ANDES source code folder, do:
+
+.. code:: bash
+
+    pip install -e .[interop]
+
 """
