8a0153eef5170f0f81de37fe43a3185d8b747869
Hantao Cui <cuihantao@gmail.com>
2022-03-26 22:43:57 -0500

    Allow setting verbose on the fly.

--- andes/main.py
+++ andes/main.py
@@ -582,8 +582,8 @@ def run(filename, input_path='', verbose=20, mp_verbose=30, ncpu=NCPUS_PHYSICAL,
 
     """
 
-    if is_interactive() and len(logger.handlers) == 0:
-        config_logger(file=False, stream_level=verbose)
+    if is_interactive():
+        config_logger(verbose, file=False)
 
     # put `input_path` back to `kwargs`
     kwargs['input_path'] = input_path
