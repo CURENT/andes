95c6a07634c9833f5610cfe8412bc43832dcbee7
Hantao Cui <cuihantao@gmail.com>
2022-04-09 20:47:44 -0500

    Move `texttable` to requirements.

--- andes/shared.py
+++ andes/shared.py
@@ -28,7 +28,7 @@ printing.options['dformat'] = '%.1f'
 printing.options['width'] = -1
 
 from andes.utils.paths import get_dot_andes_path  # NOQA
-from andes.utils.texttable import Texttable  # NOQA
+from texttable import Texttable  # NOQA
 
 # --- SYSTEM INFO ---
 NCPUS_PHYSICAL = psutil.cpu_count(logical=False)
