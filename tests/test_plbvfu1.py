e29dbfce1fb1ad6f1639714f0e2c7f2a37e88b29
Hantao <cuihantao@gmail.com>
2022-04-19 14:50:51 -0500

    Disable stability criteria in the test of PLBVFU1. Reduce disturbance in PLBVF data.

--- tests/test_plbvfu1.py
+++ tests/test_plbvfu1.py
@@ -20,6 +20,7 @@ class TestPLBVFU1(unittest.TestCase):
                        no_output=True,
                        )
         ss.TDS.config.tf = 3.0
+        ss.TDS.config.criteria = 0
         ss.TDS.run()
 
         self.assertEqual(ss.exit_code, 0)
