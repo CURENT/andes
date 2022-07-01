f3555c9937062e53df1e3d04ecf640576ea39294
Hantao <cuihantao@gmail.com>
2022-07-01 09:19:05 -0500

    Update docs for perturbation file.

--- andes/cases/ieee14/pert.py
+++ andes/cases/ieee14/pert.py
@@ -7,19 +7,36 @@ def pert(t, system):
     """
     Perturbation function called at each step.
 
-    The function is named "pert" and takes two arguments:
-    ``t`` for simulation time, and ``system`` for the system object.
+    The function needs to be named ``pert`` and takes two positional arguments:
+    ``t`` for the simulation time, and ``system`` for the system object.
+    Arbitrary logic and calculations can be performed in this function to
+    ``system``.
 
-    If the event involves switching, which will create a change
-    in the system Jacobian pattern, the ``custom_event`` flag
-    needs to be set by
+    If the perturbation event involves switching, such as disconnecting a line,
+    one will need to set the ``system.TDS.custom_event`` flag to ``True`` to
+    trigger a system connectivity checking, and Jacobian rebuilding and
+    refactorization.
+
+    To implement, add the following line to the scope where the event is
+    triggered:
 
     .. code-block :: python
 
-        self.custom_event = False
+        system.TDS.custom_event = True
 
-    The flag should only be set for the time instant when the
-    event is triggered.
+    In other scopes of the code where events are not triggered, do not add the
+    line as it may cause significantly slow-down.
+
+    The perturbation file can be supplied to the CLI using the ``--pert``
+    argument or supplied to :py:func:`andes.run` using the ``pert`` keyword.
+
+    Parameters
+    ----------
+    t : float
+        Simulation time.
+    system : andes.system.System
+        System object supplied by the simulator.
 
     """
+
     pass
