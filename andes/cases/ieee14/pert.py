d5ae4019a136b9b21349788d59e01880e4d2529b
Hantao <cuihantao@gmail.com>
2022-07-01 11:42:08 -0500

    Edit.

--- andes/cases/ieee14/pert.py
+++ andes/cases/ieee14/pert.py
@@ -9,26 +9,25 @@ def pert(t, system):
 
     The function needs to be named ``pert`` and takes two positional arguments:
     ``t`` for the simulation time, and ``system`` for the system object.
-    Arbitrary logic and calculations can be performed in this function to
+    Arbitrary logic and calculations can be applied in this function to
     ``system``.
 
     If the perturbation event involves switching, such as disconnecting a line,
     one will need to set the ``system.TDS.custom_event`` flag to ``True`` to
     trigger a system connectivity checking, and Jacobian rebuilding and
-    refactorization.
-
-    To implement, add the following line to the scope where the event is
-    triggered:
+    refactorization. To implement, add the following line to the scope where the
+    event is triggered:
 
     .. code-block :: python
 
         system.TDS.custom_event = True
 
     In other scopes of the code where events are not triggered, do not add the
-    line as it may cause significantly slow-down.
+    above line as it may cause significant slow-down.
 
     The perturbation file can be supplied to the CLI using the ``--pert``
-    argument or supplied to :py:func:`andes.run` using the ``pert`` keyword.
+    argument or supplied to :py:func:`andes.main.run` using the ``pert``
+    keyword.
 
     Parameters
     ----------
