30c9d76043567721e5c100978f98ba36a91c9a58
zaidmahmood <zaid.mahmood@okstate.edu>
2022-08-25 00:38:58 -0500

    Added hygov4.py

--- andes/models/governor/__init__.py
+++ andes/models/governor/__init__.py
@@ -5,3 +5,4 @@ from andes.models.governor.tg2 import TG2  # NOQA
 from andes.models.governor.tgov1 import TGOV1NDB  # NOQA
 from andes.models.governor.tgov1 import TGOV1, TGOV1DB, TGOV1N  # NOQA
 from andes.models.governor.hygov import HYGOV, HYGOVDB  # NOQA
+from andes.models.governor.hygov4 import HYGOV4  # NOQA
