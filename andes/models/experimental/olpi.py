b03f26c2b6e6a77e7f7a49b9448f47c3b45dbe2c
jinningwang <jinninggm@gmail.com>
2023-01-10 13:32:19 -0500

    Replace PI with PID

--- andes/models/experimental/olpi.py
+++ andes/models/experimental/olpi.py
@@ -5,8 +5,8 @@ Module for PI Controllers.
 import logging
 
 from andes.core.model import ModelData, Model
-from andes.core.param import NumParam, IdxParam
-from andes.core.block import PIController
+from andes.core.param import NumParam, IdxParam, ExtParam
+from andes.core.block import PIController, PIDController
 from andes.core.var import ExtAlgeb
 from andes.core.service import ConstService
 
@@ -24,13 +24,21 @@ class OLPIData(ModelData):
                             info='Turbine governor idx',
                             mandatory=True,
                             )
-        self.kP = NumParam(info='PI proportional coeff.',
+        self.kP = NumParam(info='PID proportional coeff.',
                            tex_name='k_P',
                            default=1,
                            )
-        self.kI = NumParam(info='PI integrative coeff.',
+        self.kI = NumParam(info='PID integrative coeff.',
                            tex_name='k_I',
                            default=1,
+                           )
+        self.kD = NumParam(info='PID derivative coeff.',
+                           tex_name='k_D',
+                           default=0,
+                           )
+        self.tD = NumParam(info='PID derivative time constant coeff.',
+                           tex_name='t_D',
+                           default=0,
                            )
 
 
@@ -49,25 +57,26 @@ class OLPIModel(Model):
                              is_input=True,
                              )
         self.pout0 = ConstService(v_str='pout',
-                               tex_name='P_{out0}',
-                               info='initial turbine governor output',
-                               )
-        self.PI = PIController(u=self.pout, kp=self.kP, ki=self.kI,
-                               tex_name='PI', info='PI', name='PI',
-                               ref=self.pout0,
-                               )
+                                  tex_name='P_{out0}',
+                                  info='initial turbine governor output',
+                                  )
+        self.PID = PIDController(u=self.pout, kp=self.kP, ki=self.kI,
+                                 kd=self.kD, Td=self.tD,
+                                 tex_name='PID', info='PID', name='PID',
+                                 ref=self.pout0,
+                                 )
 
 
 class OLPI(OLPIData, OLPIModel):
     r"""
-    Open-loop PI controller that takes Turbine Governor output as input.
+    Open-loop PI controller that takes Turbine Governor output power as input.
 
     ```
-        ┌─────────┐
-        │      ki │
-    u -> │kp + ─── │ -> y
-        │      s  │
-        └─────────┘
+        ┌────────────────────┐
+        │      ki     skd    │
+    u -> │kp + ─── + ───────  │ -> y
+        │      s    1 + sTd  │
+        └────────────────────┘
     ```
     """
 
