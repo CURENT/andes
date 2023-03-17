69274c06853ad7abfd6d31f80d3ddff82acf2f93
Hantao Cui <cuihantao@gmail.com>
2023-03-17 00:03:29 +0000

    Comment out unicode character as we are using xelatex.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -160,7 +160,7 @@ htmlhelp_basename = 'andes'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    'preamble': r'\DeclareUnicodeCharacter{2588}{-}',
+    # 'preamble': r'\DeclareUnicodeCharacter{2588}{-}',
     'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
