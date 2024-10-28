972a7489123c4c5ae8066b75aa1807d266664530
jinningwang <jinninggm@gmail.com>
2024-10-28 10:03:37 -0400

    Act connectivity manager in system.setup

--- andes/system.py
+++ andes/system.py
@@ -491,6 +491,8 @@ class System:
 
         # init connectivity manager
         self.conn.init()
+        # act on connectivity changes
+        self.conn.act()
 
         if ret is True:
             self.is_setup = True  # set `is_setup` if no error occurred
