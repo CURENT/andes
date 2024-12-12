c3a4745a90d681631349af9331824303a4bb3f8c
jinningwang <jinninggm@gmail.com>
2024-12-12 18:30:54 -0500

    Minor fix in model ACEc

--- andes/models/area.py
+++ andes/models/area.py
@@ -57,7 +57,7 @@ class ACEData(ModelData):
         ModelData.__init__(self)
         self.bus = IdxParam(model='Bus', info="bus idx for freq. measurement", mandatory=True)
         self.bias = NumParam(default=-1.0, info='bias parameter', tex_name='B',
-                             unit='MW/0.1Hz', power=True)
+                             unit='MW/0.1Hz', power=True, non_positive=True)
 
         self.busf = IdxParam(info='Optional BusFreq device idx', model='BusFreq',
                              default=None)
@@ -66,6 +66,7 @@ class ACEData(ModelData):
 class ACEc(ACEData, Model):
     """
     Area Control Error model, where bias (B) is an approximation of the actual frequency response (:math:`\\beta`).
+    Given NERC's definition, the bias should be a negative number.
 
     Continuous frequency sampling.
     System base frequency from ``system.config.freq`` is used.
@@ -110,10 +111,12 @@ class ACEc(ACEData, Model):
                           info='Bus frequency',
                           unit='p.u. (Hz)'
                           )
+        # NOTE: here, we use -abs(bias) to ensure that bias is negative to ensure compatibility
+        # with previous versions
         self.ace = Algeb(info='area control error',
                          unit='p.u. (MW)',
                          tex_name='ace',
-                         e_str='- 10 * (bias * imva) * sys_f * (f - 1) - ace',
+                         e_str='- 10 * (-abs(bias) * imva) * sys_f * (f - 1) - ace',
                          )
 
 
