30adfd1361eec0d79b8528c97bc7091bb1587cfc
jinningwang <jinninggm@gmail.com>
2025-04-28 22:48:57 -0400

    Add tests on modeldata add warning trigger

--- tests/test_numparam.py
+++ tests/test_numparam.py
@@ -3,7 +3,7 @@ import andes
 
 def test_numparam_online(caplog):
     """
-    Ensure In one model, issue a warning only once for NumParam value checks.
+    Ensure in one model, issue a warning only once for NumParam value checks.
     """
     case = andes.get_case("GBnetwork/GBnetwork.m")
     mpc = andes.io.matpower.m2mpc(case)
@@ -14,3 +14,14 @@ def test_numparam_online(caplog):
     with caplog.at_level("WARNING"):
         andes.io.matpower.mpc2system(mpc, ss)
         assert caplog.text.count("Non-zero parameter Line.x corrected to 1e-08.") == 1
+
+
+def test_modeldata_add(caplog):
+    """
+    Ensure in ModelData add, issue a warning only once for unused data.
+    """
+    with caplog.at_level("WARNING"):
+        _ = andes.load(andes.get_case('kundur/kundur_wtds.xlsx'),
+                       default_config=True,
+                       no_output=True)
+        assert caplog.text.count("WTDS: unused data {'fn': 60}.") == 1
