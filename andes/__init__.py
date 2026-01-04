d85520b6269fdb9d5a9d88f151b863c17c2ee6c5
Hantao Cui <cuihantao@gmail.com>
2026-01-04 20:03:00 +0800

    Upgrade to pyproject.toml installation framework and remove setup.py support.
    - Remove legacy files, add pyproject.toml for dependency management
    - Update Python version requirements to 3.10.
    - Adjust CI configurations for Python 3.11 and refine documentation for installation and development processes.

--- andes/__init__.py
+++ andes/__init__.py
@@ -1,5 +1,7 @@
-from . import _version
-__version__ = _version.get_versions()['version']
+try:
+    from andes._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0+unknown"
 
 from andes import io         # NOQA
 from andes import core       # NOQA
