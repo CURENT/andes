1075373583af68e6ab30ddd3c33a340c44f773f4
Hantao Cui <cuihantao@gmail.com>
2026-02-08 18:35:04 -0500

    Add back `andes.system.example`.

--- andes/system/__init__.py
+++ andes/system/__init__.py
@@ -5,8 +5,9 @@ System package.
 from andes.system.facade import (  # noqa: F401
     ExistingModels,
     System,
+    example,
     fix_view_arrays,
     import_pycode,
 )
 
-__all__ = ["System", "ExistingModels", "fix_view_arrays", "import_pycode"]
+__all__ = ["System", "ExistingModels", "example", "fix_view_arrays", "import_pycode"]
